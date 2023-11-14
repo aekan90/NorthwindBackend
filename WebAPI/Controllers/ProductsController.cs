27435994638bfb1a61a42ff30a47fd44c33fca3c
Atıf Ertugrul Kan <aekan90@hotmail.com>
2023-11-14 23:54:12 +0300

    Product API controller yazıldı.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,101 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public ActionResult GetList()
+        {
+            var result = _productService.GetList();
+            if (result.Status)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+        [HttpGet("getbyid")]
+        public ActionResult GetById(int productId)
+        {
+            var result = _productService.GetById(productId);
+            if (result.Status)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpGet("getlistbycategory")]
+        public ActionResult GetListByCategory(int categoryId)
+        {
+            var result = _productService.GetListByCategory(categoryId);
+            if (result.Status)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpPost("add")]
+        public ActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Status)
+            {
+                return Ok(result.Message);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpPost("update")]
+        public ActionResult Update(Product product)
+        {
+            var result = _productService.Update(product);
+            if (result.Status)
+            {
+                return Ok(result.Message);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpPost("delete")]
+        public ActionResult Delete(Product product)
+        {
+            var result = _productService.Delete(product);
+            if (result.Status)
+            {
+                return Ok(result.Message);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+    }
+}
